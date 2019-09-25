94eeba7d27701be62a2b860e4b498e1c782c6096
t8lohe00 <t8lohe00@students.oamk.fi>
2019-09-25 19:33:56 +0300

    menu ja sivupohjat tehty

---
+++ basic.php
@@ -0,0 +1,6 @@
+<?php include "menu.php"; ?>
+	<h2>PHP Basic</h2>
+	<p>
+		This is the PHP Basic.
+	</p>
+	<?php include "footer.php"; ?>
---
+++ footer.php
@@ -0,0 +1,6 @@
+</biv>
+<div id="footer">
+  This is the footer
+</biv>
+</body>
+</html>
